8808d6a955bda708d5742cc6586eb7cf26975d2f
GWen124 <9708281@qq.com>
2025-10-03 23:10:42 +0800

    modified:   functions/api/github/login.js

--- functions/api/github/login.js
+++ functions/api/github/login.js
@@ -1,7 +1,7 @@
 // GitHub OAuth 登录接口
 export async function onRequest(context) {
-  const clientId = '你的GitHubClientID'; // 请替换为你的GitHub OAuth App Client ID
-  const redirectUri = 'https://你的-cloudflare-pages-域名/api/github/callback';
+  const clientId = process.env.GITHUB_CLIENT_ID;
+  const redirectUri = process.env.GITHUB_REDIRECT_URI;
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo%20user`;
   return Response.redirect(githubAuthUrl, 302);
 }
